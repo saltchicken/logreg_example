800596b06634f6d14540643dbdcdfe755bc8e4b3
saltchicken <john.eicher89@gmail.com>
2025-03-08 09:32:59 -0800

    Enter labels after training

--- kmeans.py
+++ kmeans.py
@@ -16,29 +16,43 @@ def train(output_file):
     num_clusters = 2  # Adjust as needed
     kmeans = KMeans(n_clusters=num_clusters, random_state=42, n_init=10)
     kmeans.fit(X)
-    joblib.dump(kmeans, f"models/{output_file}.pkl", compress=3)
+    labels = determine_labels(kmeans, data, num_clusters)
 
-# def print_model():
-#     # Print clusters
-#     clusters = {i: [] for i in range(num_clusters)}
-#     for i, label in enumerate(kmeans.labels_):
-#         clusters[label].append(data[i][0])
-#
-#     for cluster_id, group in clusters.items():
-#         print(f"Cluster {cluster_id}:")
-#         for text in group:
-#             print(f"  - {text}")
-#         print()
-#
+    kmeans_data = {
+        "model": kmeans,
+        "labels": labels
+    }
+
+    joblib.dump(kmeans_data, f"models/{output_file}.pkl", compress=3)
+
+def determine_labels(kmeans, data, num_clusters):
+    labels = {}
+    clusters = {i: [] for i in range(num_clusters)}
+    for i, label in enumerate(kmeans.labels_):
+        clusters[label].append(data[i][0])
+
+    for cluster_id, group in clusters.items():
+        print(f"Cluster {cluster_id}:")
+        for i, text in enumerate(group):
+            print(f"  - {text}")
+            if i >= 10:
+                break
+        label = input(f"Label for cluster {cluster_id}: ")
+        labels[cluster_id] = label
+    return labels
+
+
 def predict(text):
     model = SentenceTransformer("all-MiniLM-L6-v2")  # all-mpnet-base-v2
     text_vector = model.encode(text)
-    kmeans = joblib.load("models/kmeans_model.pkl")
+    kmeans_data = joblib.load("models/kmeans_model.pkl")
+    kmeans = kmeans_data["model"]
+    labels = kmeans_data["labels"]
     cluster_id = kmeans.predict([text_vector])[0]
-    return cluster_id
+    return labels[cluster_id]
 
 
 if __name__ == "__main__":
-    # train("model")
-    prediction = predict("What was the weather today?")
-    print(prediction)
+    train("kmeans_model")
+    # prediction = predict("traffic?")
+    # print(prediction)
